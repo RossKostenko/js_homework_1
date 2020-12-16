c4da26b795acf34e7bdcd7263c409d9d2ed1c7ef
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-16 19:04:05 +0200

    Update and rename task_1.js to homework_1.js

---
+++ homework_1.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  return position + (2*roll);
+}
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet(name, owner) {
+  if(name === owner) {
+    return('Hello boss');
+  } else {
+    return('Hello guest');
+  }
+}
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+  return Math.floor (time / 2);
+}
