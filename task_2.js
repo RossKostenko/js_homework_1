2fc2914896bb3b8839b3febfb0f0536d8a3347dc
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-16 17:37:17 +0200

    Update task_2.js

--- task_2.js
+++ task_2.js
@@ -1,6 +1,5 @@
 //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
-function move (position, roll) {
-  const move = (position + (2*roll));
-  return move;
+function move(position, roll) {
+  return position + (2*roll));
 }
