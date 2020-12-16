5c0a49ae026b56bd59c056dc2dab69669271c122
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-16 17:36:24 +0200

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -1,7 +1,6 @@
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    let goals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-    return goals;
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
