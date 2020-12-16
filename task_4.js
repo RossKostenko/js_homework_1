ef675a213fdf9f4eac89e32a3be9e5c8ae640f15
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-16 18:01:19 +0200

    Update task_4.js

--- task_4.js
+++ task_4.js
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
 function litres(time) {
-  var litre = Math.floor (time / 2);
-  return litre;
+  return Math.floor (time / 2);
 }
