46b1a12e53f93d22c9ea1746f7d964a98dd09715
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-17 18:24:07 +0200

    Delete task_3.js

--- task_3.js
+++
@@ -1,9 +0,0 @@
-// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
-
-function greet(name, owner) {
-  if(name === owner) {
-    return('Hello boss');
-  } else {
-    return('Hello guest');
-  }
-}
