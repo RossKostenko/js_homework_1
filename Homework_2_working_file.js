c9200a6afe2fcf15d3b86243ff7e414f220b202f
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-21 16:06:23 +0200

    Update Homework_2_working_file.js

--- Homework_2_working_file.js
+++ Homework_2_working_file.js
@@ -145,3 +145,28 @@ function firstNonConsecutive(arr) {
     }
   }
 }
+
+
+https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+function circleCircumference(circle) {
+  return (2 * Math.Pi * circle.radius);
+}
+
+https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+function giveMeFive(obj){
+  //coding here
+  for (let key in obj){
+    return obj[key];
+    } 
+  
+}
