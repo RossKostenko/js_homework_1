c501c697be6d64d9deecf6f17344b9e122f4c931
Ross <73292006+RossKostenko@users.noreply.github.com>
2020-12-21 18:23:21 +0200

    Update Hometask_3.js

--- Hometask_3.js
+++ Hometask_3.js
@@ -8,12 +8,6 @@ class SmallestIntegerFinder {
 
 https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 
-function circleCircumference(circle) {
-  return 2 * Math.Pi * circle.radius;
-}
-
-https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
-
 class Shark extends Animal {
   constructor(name, age, status) {
     super(name, age, 0, "shark", status);
@@ -25,18 +19,16 @@ class Cat extends Animal {
     super(name, age, 4, "cat", status);
   }
   introduce(){
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+    return super.introduce() + "  Meow meow!";
   }
 }
 
 class Dog extends Animal {
   constructor(name, age, status, master) {
-    super(name, age, 4, "dog", status, master);
+    super(name, age, 4, "dog", status);
+    this.master = master;
   }
-  
   greetMaster(){
     return 'Hello ' + this.master;
   }
 }
-
-
